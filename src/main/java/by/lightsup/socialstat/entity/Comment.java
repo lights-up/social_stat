c82dc616831ecaf4424a9bbb2b30173b69fb63dd
m.girilyuk <barcanmessi@gmail.com>
2015-07-12 02:12:04 +0300

    Apache tiles dependencies added. Resolved checkstyle issues.

--- src/main/java/by/lightsup/socialstat/entity/Comment.java
+++ src/main/java/by/lightsup/socialstat/entity/Comment.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package by.lightsup.socialstat.entity;
 
@@ -10,66 +10,74 @@ import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 
 public class Comment {
-	private String idComment;
-	private String text;
-	private ShortUser user;
-	public String getIdComment() {
-		return idComment;
-	}
-	public void setIdComment(String idComment) {
-		this.idComment = idComment;
-	}
-	public String getText() {
-		return text;
-	}
-	public void setText(String text) {
-		this.text = text;
-	}
-	public ShortUser getUser() {
-		return user;
-	}
-	public void setUser(ShortUser user) {
-		this.user = user;
-	}
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((idComment == null) ? 0 : idComment.hashCode());
-		result = prime * result + ((text == null) ? 0 : text.hashCode());
-		result = prime * result + ((user == null) ? 0 : user.hashCode());
-		return result;
-	}
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Comment other = (Comment) obj;
-		if (idComment == null) {
-			if (other.idComment != null)
-				return false;
-		} else if (!idComment.equals(other.idComment))
-			return false;
-		if (text == null) {
-			if (other.text != null)
-				return false;
-		} else if (!text.equals(other.text))
-			return false;
-		if (user == null) {
-			if (other.user != null)
-				return false;
-		} else if (!user.equals(other.user))
-			return false;
-		return true;
-	}
-	
-	
-	/*
-	 *  "created_time": "1280780324",
+    private String idComment;
+    private String text;
+    private ShortUser user;
+
+    public String getIdComment() {
+        return idComment;
+    }
+
+    public void setIdComment(String idComment) {
+        this.idComment = idComment;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public ShortUser getUser() {
+        return user;
+    }
+
+    public void setUser(ShortUser user) {
+        this.user = user;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((idComment == null) ? 0 : idComment.hashCode());
+        result = prime * result + ((text == null) ? 0 : text.hashCode());
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Comment other = (Comment) obj;
+        if (idComment == null) {
+            if (other.idComment != null)
+                return false;
+        } else if (!idComment.equals(other.idComment))
+            return false;
+        if (text == null) {
+            if (other.text != null)
+                return false;
+        } else if (!text.equals(other.text))
+            return false;
+        if (user == null) {
+            if (other.user != null)
+                return false;
+        } else if (!user.equals(other.user))
+            return false;
+        return true;
+    }
+
+
+    /*
+     *  "created_time": "1280780324",
             "text": "Really amazing photo!",
             "from": {
                 "username": "snoopdogg",
@@ -78,23 +86,23 @@ public class Comment {
                 "full_name": "Snoop Dogg"
             },
             "id": "420"
-	 */
-	public static Comment newInstance(JSONObject jsonObject){
-		Comment comment = new Comment();
-		comment.setIdComment(jsonObject.get("id").toString());
-		comment.setText(jsonObject.get("text").toString());
-		comment.setUser(ShortUser.newInstance((JSONObject)jsonObject.get("from")));
-		return comment;
-	}
-	
-	public static List<Comment> getListComments(JSONArray jsonArray){
-		List<Comment> comments = new ArrayList<>();
-		Comment comment = null;
-		for (Object obj : jsonArray) {
-			JSONObject object = (JSONObject) obj;
-			comment = Comment.newInstance(object);
-			comments.add(comment);
-		}
-		return comments;
-	}
+     */
+    public static Comment newInstance(JSONObject jsonObject) {
+        Comment comment = new Comment();
+        comment.setIdComment(jsonObject.get("id").toString());
+        comment.setText(jsonObject.get("text").toString());
+        comment.setUser(ShortUser.newInstance((JSONObject) jsonObject.get("from")));
+        return comment;
+    }
+
+    public static List<Comment> getListComments(JSONArray jsonArray) {
+        List<Comment> comments = new ArrayList<>();
+        Comment comment = null;
+        for (Object obj : jsonArray) {
+            JSONObject object = (JSONObject) obj;
+            comment = Comment.newInstance(object);
+            comments.add(comment);
+        }
+        return comments;
+    }
 }
--- src/main/java/by/lightsup/socialstat/entity/Media.java
+++ src/main/java/by/lightsup/socialstat/entity/Media.java
@@ -10,109 +10,110 @@ import java.util.List;
  * Created by note on 24.05.2015.
  */
 public class Media {
-	private String idMedia;
-	private ShortUser user;
-	private List<Like> likes;
-	private List<Comment> comments;
+    private String idMedia;
+    private ShortUser user;
+    private List<Like> likes;
+    private List<Comment> comments;
 
-	private Media() {
-		likes = new ArrayList<>();
-		comments = new ArrayList<>();
-	}
+    private Media() {
+        likes = new ArrayList<>();
+        comments = new ArrayList<>();
+    }
 
-	public String getIdMedia() {
-		return idMedia;
-	}
+    public String getIdMedia() {
+        return idMedia;
+    }
 
-	public void setIdMedia(String idMedia) {
-		this.idMedia = idMedia;
-	}
+    public void setIdMedia(String idMedia) {
+        this.idMedia = idMedia;
+    }
 
-	public ShortUser getUser() {
-		return user;
-	}
+    public ShortUser getUser() {
+        return user;
+    }
 
-	public void setUser(ShortUser user) {
-		this.user = user;
-	}
+    public void setUser(ShortUser user) {
+        this.user = user;
+    }
 
-	public List<Like> getLikes() {
-		return likes;
-	}
+    public List<Like> getLikes() {
+        return likes;
+    }
 
-	public void setLikes(List<Like> likes) {
-		this.likes = likes;
-	}
+    public void setLikes(List<Like> likes) {
+        this.likes = likes;
+    }
 
-	public List<Comment> getComments() {
-		return comments;
-	}
+    public List<Comment> getComments() {
+        return comments;
+    }
 
-	public void setComments(List<Comment> comments) {
-		this.comments = comments;
-	}
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((comments == null) ? 0 : comments.hashCode());
-		result = prime * result + ((idMedia == null) ? 0 : idMedia.hashCode());
-		result = prime * result + ((likes == null) ? 0 : likes.hashCode());
-		result = prime * result + ((user == null) ? 0 : user.hashCode());
-		return result;
-	}
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((comments == null) ? 0 : comments.hashCode());
+        result = prime * result + ((idMedia == null) ? 0 : idMedia.hashCode());
+        result = prime * result + ((likes == null) ? 0 : likes.hashCode());
+        result = prime * result + ((user == null) ? 0 : user.hashCode());
+        return result;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Media other = (Media) obj;
-		if (comments == null) {
-			if (other.comments != null)
-				return false;
-		} else if (!comments.equals(other.comments))
-			return false;
-		if (idMedia == null) {
-			if (other.idMedia != null)
-				return false;
-		} else if (!idMedia.equals(other.idMedia))
-			return false;
-		if (likes == null) {
-			if (other.likes != null)
-				return false;
-		} else if (!likes.equals(other.likes))
-			return false;
-		if (user == null) {
-			if (other.user != null)
-				return false;
-		} else if (!user.equals(other.user))
-			return false;
-		return true;
-	}
-	
-	public static Media newInstance(JSONObject jsonObject){
-		Media media = new Media();
-		media.setIdMedia(jsonObject.get("id").toString());
-		media.setUser(ShortUser.newInstance((JSONObject)jsonObject.get("user")));
-		media.setLikes(Like.getLikesList((JSONArray)((JSONObject)jsonObject.get("likes")).get("data")));
-		media.setComments(Comment.getListComments((JSONArray)((JSONObject)jsonObject.get("comments")).get("data")));
-		return media;
-	}
-	public static List<Media> getMediaList(JSONArray jsonArray) {
-		List<Media> mediaList = new ArrayList<>();
-		Media media = null;
-		for (Object obj : jsonArray) {
-			JSONObject object = (JSONObject) obj;
-			media = Media.newInstance(object);
-			mediaList.add(media);
-		}
-		return mediaList;
-		
-	}
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Media other = (Media) obj;
+        if (comments == null) {
+            if (other.comments != null)
+                return false;
+        } else if (!comments.equals(other.comments))
+            return false;
+        if (idMedia == null) {
+            if (other.idMedia != null)
+                return false;
+        } else if (!idMedia.equals(other.idMedia))
+            return false;
+        if (likes == null) {
+            if (other.likes != null)
+                return false;
+        } else if (!likes.equals(other.likes))
+            return false;
+        if (user == null) {
+            if (other.user != null)
+                return false;
+        } else if (!user.equals(other.user))
+            return false;
+        return true;
+    }
+
+    public static Media newInstance(JSONObject jsonObject) {
+        Media media = new Media();
+        media.setIdMedia(jsonObject.get("id").toString());
+        media.setUser(ShortUser.newInstance((JSONObject) jsonObject.get("user")));
+        media.setLikes(Like.getLikesList((JSONArray) ((JSONObject) jsonObject.get("likes")).get("data")));
+        media.setComments(Comment.getListComments((JSONArray) ((JSONObject) jsonObject.get("comments")).get("data")));
+        return media;
+    }
+
+    public static List<Media> getMediaList(JSONArray jsonArray) {
+        List<Media> mediaList = new ArrayList<>();
+        Media media = null;
+        for (Object obj : jsonArray) {
+            JSONObject object = (JSONObject) obj;
+            media = Media.newInstance(object);
+            mediaList.add(media);
+        }
+        return mediaList;
+
+    }
 
 }
